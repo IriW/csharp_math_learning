11d719e6e3b79a772766ba84c9586c1462d2380e
Irina <32927392+IriW@users.noreply.github.com>
2021-06-21 23:47:57 +0200

    first upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace MathLearningProject
+{
+    class Program
+    {
+        static void HomeTaskDoubleRadius()
+        {
+            double a = Math.Pow(2.50, 2);
+            double b = a * Math.PI;
+            Console.WriteLine("Wyliczenie pola koła, pierwsza wersja:");
+            Console.WriteLine($"Pole koła równa się {Math.Pow(2.50, 2) * Math.PI}");
+            Console.WriteLine("Druga wersja:");
+            Console.WriteLine($"Pole koła równa się {b}");
+        }
+        static void IfElseTraining()
+        {
+            int one = 45;
+            int two = 66;
+            int three = 11;
+
+            if (one + three > two)
+            {
+                Console.WriteLine($"{one} + {three} większe niż {two}, ponieważ suma tych liczb wynosi {one + three}");
+            }
+            else
+            {
+                Console.WriteLine($"Suma {one} + {three} jest większa, niż {two}, ponieważ ich suma wynosi {one + three}");
+            }
+        }
+        static void LearnIf()
+        {
+            int a = 6;
+            int b = 8;
+            int c = 4;
+            if ((a + b + c > 10) && (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+            }
+        }
+
+        static void WhileLoop()
+        {
+            int age = 32;
+            while(age < 40)
+            {
+                Console.WriteLine($"It's still OK to be {age} ");
+                age++;
+            }
+
+            Console.WriteLine("Alternative working the same way (the result should be the same):");
+
+            int NewAge = 32;
+            do
+            {
+            Console.WriteLine($"It's still OK to be {NewAge} ");
+            NewAge++;
+            } while (NewAge < 40);
+            
+        }
+            static void ForLoop()
+            {
+                for (int index = 0; index < 10; index++)
+                {
+                Console.WriteLine($"Hello World! The index is {index}");
+                }
+
+                for (int loggerending = 10001; loggerending < 10011; loggerending++)
+                {
+                Console.WriteLine($"Logger_5433_0982340{loggerending}");
+                };
+            }
+            static void Main()
+            {
+            //HomeTaskDoubleRadius();
+            //IfElseTraining();
+            //WhileLoop();
+            ForLoop();
+            }
+    }
+}
